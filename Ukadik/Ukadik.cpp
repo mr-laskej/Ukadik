2dfde04c2b0f5e1ea37015ce7d77ce975823e528
Matyáš Laský <matyas.lasky.020@pslib.cz>
2022-03-14 13:50:14 +0100

    Přidejte soubory projektu.

---
+++ Ukadik/Ukadik.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+void bobek(int *moč) {
+	printf("promenna moc ukazuje na adresu: %p\n", moč);
+	printf("moc ukazuje v RAM na cislo: %d\n", *moč);
+	int sperma = *moč;
+	printf("pred:pomocna obsahuje: %d \n", sperma);
+	sperma++;
+	printf("po: pomocna obsahuje: %d \n", sperma);
+	*moč = sperma;
+}
+
+int main()
+{
+	int cislo;
+	int* ukazatel_na_cislo;
+	cislo = 696;
+	ukazatel_na_cislo = &cislo;
+	printf("promenna cislo je na adrese: %p\n", ukazatel_na_cislo);
+	printf("promenna cislo obsahuje: %d\n", *ukazatel_na_cislo);
+	printf("promenna cislo obsahuje: %d\n", cislo);
+	bobek(&cislo);
+	printf("promenna cislo obsahuje: %d\n", cislo);
+}
+
